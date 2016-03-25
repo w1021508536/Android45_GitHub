5318c772da78114306b3906894f73d73550efdca
w1021508536 <1021508536@qq.com>
2016-03-25 15:20:56 +0800

    yiciyou

--- app/src/main/java/com/example/android45_github/MainActivity.java
+++ app/src/main/java/com/example/android45_github/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //SDFFFDFDSFDSFD
+    //assddsadassdasdadassaddadsdadsadddas
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
